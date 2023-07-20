096202ec5702f77d8240bb8620840e2101853233
AhooraGorji <99167642+ahouragorji@users.noreply.github.com>
2023-07-20 09:21:30 +0330

    The color changes! but it is not efficient to change color based on the inner data. also, our database is not copelete and correct for german words.

--- content.js
+++ content.js
@@ -1,35 +1,46 @@
-function colorWordsInParagraphs() {
-    const paragraphs = document.querySelectorAll("p");
-    const html = document.querySelector('html');
-   
-    if(isGermanLanguage(html)){
+async function getColorForParagraph(paragraph) {
+  const wordMatchRegExp = /[^\s]+/g;
+  const words = [...paragraph.textContent.match(wordMatchRegExp) || []];
+  const fragment = document.createDocumentFragment();
+  let currentPart = '';
 
-    paragraphs.forEach((paragraph) => {
-       
-        const wordMatchRegExp = /[^\s]+/g;
-        const words = paragraph.textContent.match(wordMatchRegExp);
+  for (const word of words) {
+    const colour = await getColorForWord(word);
+    if (colour !== undefined) {
+      if (currentPart !== '') {
+        fragment.appendChild(document.createTextNode(currentPart + ' '));
+        currentPart = '';
+      }
 
-        const fragment = document.createDocumentFragment();
-        console.log(getColorForWord('ist'))
-
-        for (const word of words) {
-            colour = getColorForWord(word);
-            if(colour!=undefined){
-              console.log('added span')
-              const spanElement = document.createElement('span');
-              spanElement.style.color =colour
-              spanElement.textContent = word + ' '; 
-            fragment.appendChild(spanElement);
-            
-            }
-        }
-        
-        paragraph.innerHTML = '';
-        paragraph.appendChild(fragment);
-        
+      const spanElement = document.createElement('span');
+      spanElement.style.color = colour;
+      spanElement.textContent = word + ' ';
+      fragment.appendChild(spanElement);
+    } else {
+      currentPart += word + ' ';
     }
-    );
-}}
+  }
+
+  if (currentPart !== '') {
+    fragment.appendChild(document.createTextNode(currentPart));
+  }
+
+  paragraph.innerHTML = '';
+  paragraph.appendChild(fragment);
+}
+
+
+async function colorWordsInParagraphs() {
+  const paragraphs = document.querySelectorAll('p, i, li');
+  const html = document.querySelector('html');
+
+  if (isGermanLanguage(html)) {
+    for (const paragraph of paragraphs) {
+      await getColorForParagraph(paragraph);
+    }
+  }
+}
+
 function isGermanLanguage(element) {
     const langValue = element.getAttribute("lang");
     return langValue && langValue.startsWith("de");
@@ -50,33 +61,33 @@ function replaceUmlauts(germanWord) {
     return germanWord.replace(/[äöüÄÖÜß]/g, match => umlautMap[match] || match);
   }
   async function getColorForWord(word) {
-    // Convert the word to lowercase and remove trailing punctuation marks
+ 
     const cleanedWord = replaceUmlauts(word.toLowerCase().replace(/[.,?!]+$/, ''));
   
     return new Promise((resolve) => {
-      // Load the CSV data from chrome.storage
+   
       chrome.storage.local.get("gendersData", function (result) {
         const wordGenderPairs = result.gendersData || [];
   
-        // First, check for an exact match
+        
         const wordData = wordGenderPairs.find(data => data.word === cleanedWord);
         if (wordData) {
-          // Return the color based on the gender
+          
           switch (wordData.gender) {
             case "m":
-              resolve("#17a2b8"); // Masculine color: #17a2b8 (Turquoise)
+              resolve("#17a2b8"); 
               break;
             case "f":
-              resolve("#dc3445"); // Feminine color: #dc3445 (Crimson)
+              resolve("#dc3445"); 
               break;
             case "n":
-              resolve("#28a745"); // Neutral color: #28a745 (Green)
+              resolve("#28a745"); 
               break;
             default:
               resolve(undefined);
           }
         } else {
-          // If no exact match, check for words at the end of other words
+
           const inferredGenderData = wordGenderPairs.find(data => cleanedWord.endsWith(data.word));
           if (inferredGenderData) {
             // Return the inferred gender color
@@ -93,7 +104,8 @@ function replaceUmlauts(germanWord) {
               default:
                 resolve(undefined);
             }
-          } else {
+          }
+           {
             // Word not found in the CSV, resolve with undefined
             resolve(undefined);
           }
